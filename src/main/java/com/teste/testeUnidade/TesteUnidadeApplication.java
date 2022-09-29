8773688a6cc133c79adb0b1658fbf0d3b35f1fe0
jsuisjuan <juanc.s.delima@gmail.com>
2022-09-29 20:30:25 -0300

    rodando teste app java

---
+++ src/main/java/com/teste/testeUnidade/TesteUnidadeApplication.java
@@ -0,0 +1,23 @@
+package com.teste.testeUnidade;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUnidadeApplication {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Digite o CPF:");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String cpf = reader.readLine();
+		
+		System.out.println("O CPF digitado é: " + (ValidadorCpf.validar(cpf) ? "Valido" : "Invalido"));
+	}
+
+}
