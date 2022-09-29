d5d80492a972647030eaa09aef500d1c7612b9e8
jsuisjuan <juanc.s.delima@gmail.com>
2022-09-29 20:38:21 -0300

    mudando imagem da pipeline

--- src/test/java/com/teste/testeUnidade/TesteUnidadeApplicationTests.java
+++ src/test/java/com/teste/testeUnidade/TesteUnidadeApplicationTests.java
@@ -2,7 +2,6 @@ package com.teste.testeUnidade;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
